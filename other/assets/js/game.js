41f6bb9a66492108269cf0cee3fe24ec5c2e229a
Diuke <juan-pablo-duque@hotmail.com>
2020-05-29 23:25:28 +0200

    first page update

---
+++ other/assets/js/game.js
@@ -0,0 +1,37 @@
+$( document ).ready(function() {
+
+    var question = $('#question');
+    var answer = $('#answer');
+    var phrase = $('#phrase');
+
+    var phrases = [
+        "Keep trying",
+        "Not... Even... Close",
+        "Are you really trying",
+        "Nope",
+        "Noooope",
+        "Nope",
+        "Just no",
+        "C'mon...",
+        "You are getting close... Just kidding",
+        "Not quite the answer",
+        "You are getting close to being more incorrect",
+        "You can't be more incorrect"
+    ];
+    
+    var correctPhrase = "You answered correctly! "
+
+    var questions = [
+        ""
+    ]
+
+    var answers = [
+        "42",
+
+    ];
+
+    
+
+
+    console.log( "ready!" );
+});
